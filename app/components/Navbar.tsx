9cd5e6c4059b7f6e67dc3bebc6f73e0f2ae032b9
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2024-09-13 12:49:13 +0000

    modification log

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,10 +6,13 @@ import Image from "next/image";
 const Navbar: React.FC = () => {
   return (
     <nav className="bg-gray-100 dark:bg-gray-800 shadow-sm sticky top-0 z-50 flex items-center justify-between px-4 py-2">
-      <Image src={logo} alt="Logo" width={40} height={40} className="p-2" />
+      <div className="rounded-full bg-gray-900  p-2">
+        <Image src={logo} alt="Logo" className="w-12" />
+      </div>
       <ul className="flex flex-row items-center gap-6 dark:text-white">
         <li>Home</li>
         <li>About</li>
+        <li>Projects</li>
         <li>Contact</li>
         <ThemeSwitcher />
       </ul>
