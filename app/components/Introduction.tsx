4b17141da606f2114651a07d27853ab246cba2b6
Mampionona33 <ramamps33@gmai.com>
2024-09-14 22:59:57 +0300

    remplace le text intro

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,17 +1,19 @@
 const Introduction = () => {
   return (
-    <section className="min-h-screen pt-5" id="accueil">
-      <hr className="h-2 bg-neutral-900 dark:bg-neutral-100 rounded w-1/6 mb-6" />
-      <div className="flex flex-col gap-2 w-1/2">
-        <h1 className="text-3xl font-medium">
-          Je suis Mampionona, et je suis un Développeur
-        </h1>
-        <p>
-          Développeur spécialisé dans la création d'applications complexes
-          telles que les ERP et les SIRH. J'utilise des technologies telles que
-          React, Node.js, Laravel et MongoDB pour concevoir des solutions
-          robustes et innovantes. Découvrez mes projets dans ce portfolio.
-        </p>
+    <section className="min-h-screen flex items-center" id="accueil">
+      <div>
+        <hr className="h-2 bg-neutral-900 dark:bg-neutral-100 rounded w-1/6 mb-6" />
+        <div className="flex flex-col gap-2 w-1/2">
+          <h1 className="text-3xl font-medium">
+            Mampionona - Développeur Web Full Stack
+          </h1>
+          <p>
+            Spécialisé dans la création d'applications complexes telles que les
+            ERP et les SIRH, j'utilise des technologies comme React, Node.js,
+            Laravel et MongoDB pour concevoir des solutions performantes et
+            innovantes. Explorez mes projets dans ce portfolio.
+          </p>
+        </div>
       </div>
     </section>
   );
