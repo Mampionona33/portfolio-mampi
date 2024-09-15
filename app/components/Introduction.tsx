ab495ba071a672444c3b0e54f77b6b734d39d18d
Mampionona33 <ramamps33@gmai.com>
2024-09-15 20:01:53 +0300

    correction responsive

--- app/components/Introduction.tsx
+++ app/components/Introduction.tsx
@@ -1,18 +1,14 @@
 const Introduction = () => {
   return (
     <section className="min-h-screen flex items-center" id="accueil">
-      <div>
+      <div className="px-12">
         <hr className="h-2 bg-neutral-900 dark:bg-neutral-100 rounded w-1/6 mb-6" />
-        <div className="flex flex-col gap-2 w-1/2">
+        <div className="flex flex-col gap-2 w-full sm:w-full md:w-1/2">
           <h1 className="text-3xl font-medium">
             Mampionona - Développeur Web Full Stack
           </h1>
           <p>
-            {`Spécialisé dans la création d'applications complexes telles que les
-            ERP et les SIRH, j'utilise des technologies comme React, Node.js,
-            Laravel et MongoDB pour concevoir des solutions performantes et
-            innovantes. Explorez mes projets dans ce portfolio.
-            `}
+            {`Spécialisé dans la création d'applications complexes telles que les ERP et les SIRH, j'utilise des technologies comme React, Node.js, Laravel et MongoDB pour concevoir des solutions performantes et innovantes. Explorez mes projets dans ce portfolio.`}
           </p>
         </div>
       </div>
